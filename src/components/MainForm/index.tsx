46fbfbf18210010c49ab3f6f4da7a1e346427142
Gabriel Magnago <gabriel.magnago14@gmail.com>
2025-10-14 19:18:00 -0300

    Ajustando o form principal

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,11 +3,50 @@ import DefaultInput from '../DefaultInput'
 import Cycles from '../Cycles'
 import DefaultButton from '../DefaultButton'
 import { PlayCircleIcon } from 'lucide-react'
+import { useRef } from 'react'
+import type { TaskModel } from '../../models/TaskModel'
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
 
 const MainForm = () => {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      staartDate: Date.now(),
+      completeDate: null,
+      interruptedDate: null,
+      duration: 1,
+      type: 'workTime',
+    }
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // validar depois
+        secondsRemaining,
+        formattedSecondRemaining: '00:00', // colocar a função de tempo dps 
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
   }
 
   return (
@@ -18,6 +57,7 @@ const MainForm = () => {
           id="menuInput"
           type="text"
           placeholder="Digite algo"
+          ref={taskNameInput}
         />
       </div>
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,8 @@
 import Container from '../../components/Container'
 import { CountDown } from '../../components/CountDown'
 import MainForm from '../../components/MainForm'
-import type { TaskStateModel } from '../../models/TaskStateModel'
 import MainTemplate from '../../templates/MainTemplate'
 
-type HomeProps = {
-  state: TaskStateModel,
-  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>,
-}
 
 const Home = () => {
 
